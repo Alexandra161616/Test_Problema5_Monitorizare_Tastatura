7401e1c10f349d17b7ec89266a7b543ce7e8c657
Alexandra <georgianaalexandrac@gmail.com>
2023-07-07 05:00:23 +0300

    Add project files.

---
+++ Test_Problema5/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Test_Problema5
+{
+    public partial class Form1 : Form
+    {
+
+        private LowLevelKeyboardListener _listener;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _listener = new LowLevelKeyboardListener();
+            _listener.OnKeyPressed += _listener_OnKeyPressed;
+
+            _listener.HookKeyboard();
+        }
+
+        private void _listener_OnKeyPressed(object sender, KeyPressedArgs e)
+        {
+            this.textBox1.Text += e.KeyPressed.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _listener.UnHookKeyboard();
+        }
+    }
+}
+
---
+++ Test_Problema5/LowLevelKeyboardListener.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+
+namespace Test_Problema5
+{
+    public class LowLevelKeyboardListener
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        //user32.dll e un modul care contine functiile Windows API adica Windows user interface (Window handling, functii basic UI, etc).
+        //user32.dll e un system process de care ai nevoie pentru ca PC ul tau sa functioneze cum trebuie. Nu trebuie sters.
+        //user32.dll este o fila executabila in hard drive ul computerului tau. Aceasta contine cod masina. Daca pornesti software ul Windows User API pe PC ul tau, comenzile care se afla in user32.dll vor fi executate de PC ul tau. Din acest motiv, file ul e incarcat in memoria principala (RAM) si ruleaza acolo ca un proces Windows User API Client DLL (adica task)
+        //dynamic-link library =DLL
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        //CharSet = Indica cum sa ii pasezi parametri string metodei si controleaza numele.
+        //SetLastError = Indica daca callee da eroare error(SetLastError in Windows sau eroare in alte platforme) inainte sa se intoarca din metoda atribuita
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public event EventHandler<KeyPressedArgs> OnKeyPressed;
+
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        public LowLevelKeyboardListener()
+        {
+            _proc = HookCallback;
+        }
+
+        public void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public void UnHookKeyboard()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                if (OnKeyPressed != null) { OnKeyPressed(this, new KeyPressedArgs(KeyInterop.KeyFromVirtualKey(vkCode))); }
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+    }
+
+    public class KeyPressedArgs : EventArgs
+    {
+        public Key KeyPressed { get; private set; }
+
+        public KeyPressedArgs(Key key)
+        {
+            KeyPressed = key;
+        }
+    }
+}
